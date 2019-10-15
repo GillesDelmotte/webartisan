5826b47422e4c521527c8d5bda5b96ba65645a08
gilles delmotte <gilles.delmotte@outlook.be>
2019-10-15 14:19:53 +0200

    add jobs cpt

--- functions/cpt.php
+++ functions/cpt.php
@@ -7,10 +7,24 @@
                 'singular-name' => 'actualité',
                 'add_new_item' => 'ajouter une nouvelle actualité'
             ],
-            'description' => 'All the actualities from la cité école vivante',
+            'description' => 'Toutes les actus de webartisan',
             'public' => true,
             'menu_position' => 5,
             'menu_icon' => 'dashicons-welcome-write-blog',
+            'rewrite' => ['slug' => 'actualities'],
+            'taxonomies' => array('post_tag')
+        ]);
+
+        register_post_type('jobs', [
+            'label' => 'offres d‘emplois',
+            'labels' => [
+                'singular-name' => 'offre d‘emploi',
+                'add_new_item' => 'ajouter une nouvelle offre'
+            ],
+            'description' => 'Toutes les offres d‘emplois de webartian',
+            'public' => true,
+            'menu_position' => 5,
+            'menu_icon' => 'dashicons-businessman',
             'rewrite' => ['slug' => 'actualities'],
             'taxonomies' => array('post_tag')
         ]);
