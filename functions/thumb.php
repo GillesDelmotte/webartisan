6e0ccfe75bfdf8b5dab39de53f7ab95941663660
gilles delmotte <gilles.delmotte@outlook.be>
2019-10-15 17:07:01 +0200

    add 285x350 size

--- functions/thumb.php
+++ functions/thumb.php
@@ -8,4 +8,5 @@
 
     add_image_size('thumb_1184x500', 1184, 500, true);
     add_image_size('thumb_690x300', 690, 300, array( 'left', 'top' ));
+    add_image_size('thumb_285x350', 285, 350, true);
 ?>
