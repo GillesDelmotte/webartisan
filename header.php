4a6f6baea47d1c5e23f9ea39e986707bac2f7059
gilles delmotte <gilles.delmotte@outlook.be>
2019-10-24 09:11:11 +0200

    add meta desc + keywords

--- header.php
+++ header.php
@@ -4,8 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="<?= get_field("meta__description");?>">
+    <meta name="keywords" content="<?= get_field("meta__keywords");?>">
     <link rel="stylesheet" href="<?= assets('./dist/app.css'); ?>">
-    <title>Webartisan</title>
+    <title><?php the_title(); ?> - Webartisan</title>
 </head>
 <body>
 
@@ -14,7 +16,7 @@
         <?php $logo = get_field('logo__dark', 'options'); ?>
         <?= wp_get_attachment_image( $logo, "full" ); ?>
     </a>
-    <div class="header__nav">
+    <div class="header__nav" role="navigation">
         <?php wp_nav_menu(); ?>
         <a class="header__connexion" href="<?= wp_login_url( $redirect ); ?> ">Connexion</a>
     </div>
