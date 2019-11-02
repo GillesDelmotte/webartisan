2fc828a5651e7678e28e6b44dcefc5895b13772b
gilles delmotte <gilles.delmotte@outlook.be>
2019-11-02 14:52:52 +0100

    Update app.js

---
+++ src/app.js
@@ -0,0 +1,20 @@
+//const form = document.querySelector('.js-form-login');
+
+// form.addEventListener('submit', function (e) {
+//     e.preventDefault();
+//     e.stopPropagation();
+
+//     //console.log(document.querySelector('#securityConnection').value);
+
+//     var data = {
+//         action: 'ajax_login',
+//         username: document.querySelector('#nameConnection').value,
+//         password: document.querySelector('#passwordConnection').value,
+//         security: document.querySelector('#securityConnection').value
+//     };
+
+//     var request = new XMLHttpRequest();
+//     request.open('POST', 'ajax_login', true);
+//     request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+//     request.send(data);
+// })
