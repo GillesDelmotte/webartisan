b274c7702f8cb93349ba073b5be519155f62dc5f
gilles delmotte <gilles.delmotte@outlook.be>
2019-11-06 10:32:43 +0100

    add newletter functions

--- footer.php
+++ footer.php
@@ -1,15 +1,17 @@
  <div class="pageFooter__clip">
-    <div class="footer__nav">
-        <?php wp_nav_menu(); ?>  
-    </div>
-    <div class="footer__newsletter">
-        <h2 class="footer__newsletter__title">S'abonner à la newsletter</h2>
-        <p class="footer__newsletter__content">Pour recevoir toutes les informations par e-mail n’hésiter pas vous abonnez à notre newsletter</p>
-        <form action="" method="POST" class="footer__newsletter__form">
-            <input type="email" name="email" class="footer__newsletter__form__input"><button class="footer__newsletter__form__button"><span class="sr-only">Rechercher</span><?php require get_template_directory().'/dist/icons/correct.svg';?></button>
-        </form>
-    </div>
-</div>
-</footer>
-<script src="<?= assets('./dist/app.js'); ?>"></script>
-</html>
+     <div class="footer__nav">
+         <?php wp_nav_menu(); ?>
+     </div>
+     <div class="footer__newsletter">
+         <h2 class="footer__newsletter__title">S'abonner à la newsletter</h2>
+         <p class="footer__newsletter__content">Pour recevoir toutes les informations par e-mail n’hésiter pas vous abonnez à notre newsletter</p>
+         <form action="<?= admin_url('admin-post.php'); ?>" method="POST" class="footer__newsletter__form">
+             <input type="hidden" name="action" value="post_newsletter">
+             <input type="email" name="email" class="footer__newsletter__form__input"><button class="footer__newsletter__form__button"><span class="sr-only">Rechercher</span><?php require get_template_directory() . '/dist/icons/correct.svg'; ?></button>
+         </form>
+     </div>
+ </div>
+ </footer>
+ <script src="<?= assets('./dist/app.js'); ?>"></script>
+
+ </html>
--- functions.php
+++ functions.php
@@ -6,14 +6,14 @@ require_once 'functions/thumb.php';
 require_once 'functions/paginate.php';
 
 
-function assets($path){
-    return get_template_directory_uri() . '/' . trim($path, '/');
+function assets($path)
+{
+	return get_template_directory_uri() . '/' . trim($path, '/');
 };
 
-if( function_exists('acf_add_options_page') ) {
-	
+if (function_exists('acf_add_options_page')) {
+
 	acf_add_options_page();
-	
 }
 
 /* 
@@ -27,82 +27,105 @@ if( function_exists('acf_add_options_page') ) {
  * @return array $classes
  */
 
-function be_menu_item_classes( $classes, $item, $args ) {
-	if( 'header' !== $args->theme_location )
+function be_menu_item_classes($classes, $item, $args)
+{
+	if ('header' !== $args->theme_location)
 		return $classes;
-	if( ( is_singular( 'post' ) || is_category() || is_tag() ) && 'Blog' == $item->title )
+	if ((is_singular('post') || is_category() || is_tag()) && 'Blog' == $item->title)
 		$classes[] = 'current-menu-item';
-		
-	if( ( is_singular( 'code' ) || is_tax( 'code-tag' ) ) && 'Code' == $item->title )
+
+	if ((is_singular('code') || is_tax('code-tag')) && 'Code' == $item->title)
 		$classes[] = 'current-menu-item';
-		
-	if( is_singular( 'projects' ) && 'Case Studies' == $item->title )
+
+	if (is_singular('projects') && 'Case Studies' == $item->title)
 		$classes[] = 'current-menu-item';
-		
-	return array_unique( $classes );
+
+	return array_unique($classes);
 }
-add_filter( 'nav_menu_css_class', 'be_menu_item_classes', 10, 3 );
+add_filter('nav_menu_css_class', 'be_menu_item_classes', 10, 3);
 
 
-function post_login(){
+function post_login()
+{
 
 	$info = array();
 	$info['user_login'] = $_POST['name'];
 	$info['user_password'] = $_POST['password'];
 	$info['remember'] = true;
 
-	$user_signon = wp_signon( $info, false );
+	$user_signon = wp_signon($info, false);
 
-	
-	if(!is_wp_error($user_signon)){
-        wp_redirect( home_url() ); exit;
-    }else{
-        wp_redirect( home_url( '/connexion-inscription' ) ); exit;	
+
+	if (!is_wp_error($user_signon)) {
+		wp_redirect(home_url());
+		exit;
+	} else {
+		wp_redirect(home_url('/connexion-inscription'));
+		exit;
 	}
-
-	
 }
 
 add_action('admin_post_post_login', 'post_login');
 add_action('admin_post_nopriv_post_login', 'post_login');
 
 
-function post_newOffer(){
+function post_newOffer()
+{
 
-	if(!is_user_logged_in()){
-		wp_redirect( home_url( '/offres-demplois-stage' ) ); exit;
-    }
+	if (!is_user_logged_in()) {
+		wp_redirect(home_url('/offres-demplois-stage'));
+		exit;
+	}
 
 	$my_post = array(
 		'post_type' => 'jobs',
 		'post_title' => $_POST['title'],
 		'post_status' => 'publish',
-	  );
-	   
-	  // Insert the post into the database
-	 $post_id =  wp_insert_post( $my_post );
+	);
 
-	 $tags = explode(",", $_POST['tags']);
+	// Insert the post into the database
+	$post_id =  wp_insert_post($my_post);
 
-	 $array = [];
+	$tags = explode(",", $_POST['tags']);
 
-	 foreach($tags as $tag){
+	$array = [];
+
+	foreach ($tags as $tag) {
 		$array[] = $tag;
-	 }
+	}
 
-	 wp_set_post_tags( $post_id, $array);
+	wp_set_post_tags($post_id, $array);
 
-	 update_field( "offer__name", $_POST['name'], $post_id );
-	 update_field( "offer__type", $_POST['type'], $post_id );
-	 update_field( "offer__location", $_POST['city'], $post_id );
-	 update_field( "offer__phone", $_POST['tel'], $post_id );
-	 update_field( "offer__email", $_POST['email'], $post_id );
-	 update_field( "offer__content", $_POST['desc'], $post_id );
+	update_field("offer__name", $_POST['name'], $post_id);
+	update_field("offer__type", $_POST['type'], $post_id);
+	update_field("offer__location", $_POST['city'], $post_id);
+	update_field("offer__phone", $_POST['tel'], $post_id);
+	update_field("offer__email", $_POST['email'], $post_id);
+	update_field("offer__content", $_POST['desc'], $post_id);
 
 
-	 wp_redirect( home_url( '/offres-demplois-stage' ) ); exit;
-	
+	wp_redirect(home_url('/offres-demplois-stage'));
+	exit;
 }
 
 add_action('admin_post_post_newOffer', 'post_newOffer');
 add_action('admin_post_nopriv_post_newOffer', 'post_newOffer');
+
+
+function post_newsletter()
+{
+	$email = $_POST['email'];
+	//update_field('newsletter__rep', $email, 'options');
+
+	$row = array(
+		'email' => $email,
+	);
+
+	add_row('newsletter__rep', $row, 'options');
+
+	wp_redirect(home_url('/'));
+	exit;
+}
+
+add_action('admin_post_post_newsletter', 'post_newsletter');
+add_action('admin_post_nopriv_post_newsletter', 'post_newsletter');
