a244dd9e993389383210b59e0af46fccab658c6b
gilles delmotte <gilles.delmotte@outlook.be>
2019-11-06 12:04:34 +0100

    add highlighted article on homepage

--- templates/template-homepage.php
+++ templates/template-homepage.php
@@ -1,22 +1,48 @@
 <?php /*Template name: page d'accueil*/ ?>
-<?php get_header(); ?> 
+<?php get_header(); ?>
 <div class="pageHeader homepage">
     <div class="pageHeader__clip">
         <h1 class="pageHeader__title">
-            <?= get_bloginfo( $show = 'name' ); ?>
+            <?= get_bloginfo($show = 'name'); ?>
         </h1>
         <p class="pageHeader__content">
             <?= get_field('header__description'); ?>
         </p>
     </div>
 </div>
-<!-- <section class="homepage__actu">
-    <h2 class="homepage__actu__title">Article à la une</h2>
-    <article class="homepage__actu__article">
-        <h3 class="homepage__actu__article__title"></h3>
+<section class="highlighted">
+    <h2 class="highlighted__title">Article à la une</h2>
+    <?php
+    $post = get_field('article', 'options');
+    setup_postdata($post);
+    ?>
+    <article class="highlighted__article">
+        <?php
+        $img = get_field('actu__img');
+        $size = 'thumb_690x300'
+        ?>
+        <?= wp_get_attachment_image($img, $size); ?>
+        <div class="highlighted__article__content">
+            <h3 class="highlighted__article__title">
+                <a href="<?= the_permalink(); ?>"><?= the_title(); ?></a>
+            </h3>
+            <div class="highlighted__article__infos">
+                <span class="article__date">Le <?= get_the_date(); ?></span><span class="article__author"><?= get_author_name(); ?></span>
+            </div>
+            <p class="highlighted__article__resume"><?= get_field('actu__resume'); ?></p>
+            <ul class="highlighted__article__tags">
+                <?php $tags = get_the_tags(get_the_ID()); ?>
+                <?php foreach ($tags as $tag) : ?>
+                    <li class="tag <?= $tag->slug; ?>">
+                        <?= $tag->name; ?>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+        </div>
     </article>
-</section> -->
+    <a href="<?= the_permalink(12); ?>" class="highlighted__link">Voir tous les articles</a>
+</section>
 </body>
 <footer class="pageFooter homepage">
-    
-<?php get_footer(); ?> 
+
+    <?php get_footer(); ?>
